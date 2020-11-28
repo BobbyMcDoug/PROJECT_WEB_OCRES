7a86347d0467d568a8e2d46b06bf73fb26c298b4
Jrmeyes <jeremy.milecki@hotmail.fr>
2020-11-28 18:38:05 +0100

    Ajout component + widget courbe ventes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LineChart from './components/LineChart';
+
+const profils = [
+  {
+    nom: "Milecki",
+    prenom: "Jeremy",
+    date_naiss: "13 Novembre",
+    couleur: "red",
+  },
+  {
+    nom: "Bourgeois",
+    prenom: "Antoine",
+    date_naiss: "25 Decembre",
+    couleur: "blue",
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='chart'>
+        <LineChart />
+      </div>
     </div>
   );
 }
